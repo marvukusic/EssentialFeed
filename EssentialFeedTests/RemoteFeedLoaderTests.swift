f8f858be4fd65c970e5b020f9ca6902da4cd4836
Marko Vukušić <marko@codesplitters.com>
2022-07-26 18:08:56 +0200

    Make HTTPClient a protocol

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -7,49 +7,43 @@
 
 import XCTest
 
+protocol HTTPClient {
+    func get(from url: URL)
+}
+
+class HTTPClientSpy: HTTPClient {
+    var requestedURL: URL?
+    
+    func get(from url: URL) {
+        requestedURL = url
+    }
+}
+
+
 class RemoteFeedLoaderTests: XCTestCase {
     
     class RemoteFeedLoader {
+        let client: HTTPClient
+        
+        init(client: HTTPClient) {
+            self.client = client
+        }
+        
         func load() {
-            HTTPClient.shared.get(from: URL(string: "https://a-url.com")!)
+            client.get(from: URL(string: "https://a-url.com")!)
         }
-    }
-    
-    class HTTPClient {
-        
-        static var shared = HTTPClient()
-        
-        func get(from url: URL) {}
-    }
-    
-    class HTTPClientSpy: HTTPClient {
-        var requestedURL: URL?
-        
-        override func get(from url: URL) {
-            requestedURL = url
-        }
-    }
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
     func test_init_doesNotRequestDataFromURL() {
         let client = HTTPClientSpy()
-        HTTPClient.shared = client
-        _ = RemoteFeedLoader()
+        _ = RemoteFeedLoader(client: client)
 
         XCTAssertNil(client.requestedURL)
     }
     
     func test_load_requestDataFromURL() {
         let client = HTTPClientSpy()
-        HTTPClient.shared = client
-        let sut = RemoteFeedLoader()
+        let sut = RemoteFeedLoader(client: client)
         
         sut.load()
         
