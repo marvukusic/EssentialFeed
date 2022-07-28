9b2c75e19d5f59a92d719a1df011272ae71aa17e
Marko Vukušić <marko@codesplitters.com>
2022-07-28 11:40:02 +0200

    Move FeedLoader components to production

---
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -0,0 +1,26 @@
+//
+//  RemoteFeedLoader.swift
+//  EssentialFeed
+//
+//  Created by Marko Vukušić on 28.07.2022.
+//
+
+import Foundation
+
+public protocol HTTPClient {
+    func get(from url: URL)
+}
+
+public final class RemoteFeedLoader {
+    private let url: URL
+    private let client: HTTPClient
+    
+    public init(url: URL, client: HTTPClient) {
+        self.client = client
+        self.url = url
+    }
+    
+    public func load() {
+        client.get(from: url)
+    }
+}
