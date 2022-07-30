27cfb12e0106abba6c3aa8744dd6c691dd7e86c2
Marko Vukušić <marko@codesplitters.com>
2022-07-30 15:34:21 +0200

    Create HTTPClient test using stubs

---
+++ EssentialFeedTests/FeedAPI/URLSessionHTTPClientTests.swift
@@ -0,0 +1,73 @@
+//
+//  URLSessionHTTPClientTests.swift
+//  EssentialFeedTests
+//
+//  Created by Marko Vukušić on 30.07.2022..
+//
+
+import XCTest
+
+class URLSessionHTTPClient {
+    let session: URLSession
+    
+    init(session: URLSession) {
+        self.session = session
+    }
+    
+    func get(from url: URL) {
+        session.dataTask(with: url).resume()
+    }
+}
+
+class URLSessionHTTPClientTests: XCTestCase {
+
+    func test_getFromURL_createsDataTaskWithURL() {
+        let url = URL(string: "https://a-url.com")!
+        let session = URLSessionSpy()
+        let sut = URLSessionHTTPClient(session: session)
+        
+        sut.get(from: url)
+        
+        XCTAssertEqual(session.receivedURLs, [url])
+    }
+    
+    func test_getFromURL_resumesDataTaskWithURL() {
+        let url = URL(string: "https://a-url.com")!
+        let session = URLSessionSpy()
+        let task = URLSessionDataTaskSpy()
+        let sut = URLSessionHTTPClient(session: session)
+        session.stub(url: url, task: task)
+        
+        sut.get(from: url)
+        
+        XCTAssertEqual(session.receivedURLs, [url])
+    }
+    
+    private class URLSessionSpy: URLSession {
+        var receivedURLs = [URL]()
+        private var stubs = [URL: URLSessionDataTask]()
+        
+        override func dataTask(with url: URL) -> URLSessionDataTask {
+            receivedURLs.append(url)
+            
+            return stubs[url] ?? FakeURLSessionDataTask()
+        }
+        
+        func stub(url: URL, task: URLSessionDataTask) {
+            stubs[url] = task
+        }
+    }
+    
+    private class FakeURLSessionDataTask: URLSessionDataTask {
+        override func resume() {
+            
+        }
+    }
+    
+    private class URLSessionDataTaskSpy: URLSessionDataTask {
+        var resumeCallCount = 0
+        override func resume() {
+            resumeCallCount += 1
+        }
+    }
+}
