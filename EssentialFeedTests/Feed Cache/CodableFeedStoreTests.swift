05ddeb42455c08986bc3e04cda4c289068f5645e
Marko Vukušić <marko@codesplitters.com>
2022-08-22 17:55:17 +0200

    Insert delivers error on insertion error

--- EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -58,11 +58,15 @@ class CodableFeedStore {
     }
     
     func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping FeedStore.InsertionCompletion) {
-        let encoder = JSONEncoder()
-        let cache = Cache(feed: try! feed.map(CodableFeedImage.init), timestamp: timestamp)
-        let encoded = try! encoder.encode(cache)
-        try? encoded.write(to: storeURL)
-        completion(nil)
+        do {
+            let encoder = JSONEncoder()
+            let cache = Cache(feed: try feed.map(CodableFeedImage.init), timestamp: timestamp)
+            let encoded = try encoder.encode(cache)
+            try encoded.write(to: storeURL)
+            completion(nil)
+        } catch {
+            completion(error)
+        }
     }
 }
 
@@ -143,7 +147,17 @@ class CodableFeedStoreTests: XCTestCase {
         XCTAssertNil(latestInsertionError, "Expected successful second insertion")
         
         expect(sut, toRetreive: .found(feed: latestFeed, timestamp: latestTimestamp))
+    }
+    
+    func test_insert_deliversErrorOnInsertionError() {
+        let invalidStoreURL = URL(string: "invalid://store-url")
+        let sut = makeSUT(storeURL: invalidStoreURL)
+        let feed = uniqueImageFeed().local
+        let timestamp = Date()
         
+        let insertionError = insert((feed: feed, timestamp: timestamp), to: sut)
+        
+        XCTAssertNotNil(insertionError, "Expected failure")
     }
     
     // - MARK: Helpers
@@ -160,7 +174,6 @@ class CodableFeedStoreTests: XCTestCase {
         
         var insertionError: Error?
         sut.insert(cache.feed, timestamp: cache.timestamp) { receivedInsertionError in
-            XCTAssertNil(receivedInsertionError, file: file, line: line)
             insertionError = receivedInsertionError
             exp.fulfill()
         }
