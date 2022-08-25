d556b4eae721707a71a56370678ecdf8b931234f
Marko Vukušić <marko@codesplitters.com>
2022-08-25 16:24:24 +0200

    Add ManagedCache and ManagedFeedImage model representations

---
+++ EssentialFeed/Feed Cache/CoreDataFeedStore.swift
@@ -0,0 +1,47 @@
+//
+//  CoreDataFeedStore.swift
+//  EssentialFeed
+//
+//  Created by Marko Vukušić on 25.08.2022.
+//
+
+import CoreData
+
+public final class CoreDataFeedStore: FeedStore {
+    //    private let container: NSPersistentContainer
+    //    private let context: NSManagedObjectContext
+    
+    public init() {}
+    
+    public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
+        
+    }
+    
+    public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
+        
+    }
+    
+    public func retrieve(completion: @escaping RetrievalCompletion) {
+        completion(.empty)
+    }
+    
+    //    private func perform(_ action: @escaping (NSManagedObjectContext) -> Void) {
+    //        let context = self.context
+    //        context.perform {
+    //            action(context)
+    //        }
+    //    }
+}
+
+private class ManagedCache: NSManagedObject {
+    @NSManaged var timestamp: Date
+    @NSManaged var feed: NSOrderedSet
+}
+
+private class ManagedFeedImage: NSManagedObject {
+    @NSManaged var id: UUID
+    @NSManaged var imageDescription: String?
+    @NSManaged var location: String?
+    @NSManaged var url: URL
+    @NSManaged var cache: ManagedCache
+}
--- EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -8,22 +8,6 @@
 import XCTest
 import EssentialFeed
 
-public final class CoreDataFeedStore: FeedStore {
-    public init() {}
-    
-    public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
-        
-    }
-    
-    public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
-        
-    }
-    
-    public func retrieve(completion: @escaping RetrievalCompletion) {
-        completion(.empty)
-    }
-}
-
 class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
     func test_retrieve_deliversEmptyOnEmptyCache() {
         let sut = makeSUT()
@@ -64,6 +48,8 @@ class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
     // - MARK: Helpers
     
     func makeSUT(file: StaticString = #file, line: UInt = #line) -> FeedStore {
+//        let storeBundle = Bundle(for: CoreDataFeedStore.self)
+//        let storeURL = URL(fileURLWithPath: "/dev/null")
         let sut = CoreDataFeedStore()
         trackForMemoryLeaks(sut, file: file, line: line)
         return sut
