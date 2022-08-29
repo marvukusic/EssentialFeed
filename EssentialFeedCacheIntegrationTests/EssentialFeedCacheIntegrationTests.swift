deb624f4343ed7341b9f68c93aafd7340d283728
Marko Vukušić <marko@codesplitters.com>
2022-08-29 15:24:33 +0200

    Add EssentialFeedCacheIntegrationTarget

---
+++ EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -0,0 +1,16 @@
+//
+//  EssentialFeedCacheIntegrationTests.swift
+//  EssentialFeedCacheIntegrationTests
+//
+//  Created by Marko Vukušić on 29.08.2022.
+//
+
+import XCTest
+
+class EssentialFeedCacheIntegrationTests: XCTestCase {
+
+    func test_deliversNoItemsOnEmptyCache() {
+        
+    }
+
+}
