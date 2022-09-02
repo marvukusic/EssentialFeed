4e65fce4c48e29225ceb0083889f5edb337bce83
Marko Vukušić <marko@codesplitters.com>
2022-09-02 15:02:00 +0200

    Add FeedViewController template code

---
+++ Prototype/Prototype/FeedViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FeedViewController.swift
+//  Prototype
+//
+//  Created by Marko Vukušić on 02.09.2022.
+//
+
+import UIKit
+
+class FeedViewController: UITableViewController {
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        tableView.dequeueReusableCell(withIdentifier: "FeedImageCell", for: indexPath)
+    }
+}
