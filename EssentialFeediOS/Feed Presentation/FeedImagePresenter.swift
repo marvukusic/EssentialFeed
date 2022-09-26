e1e6c0274d6f762eab5dfc2d4bcaf043b169e8d1
Marko Vukušić <marko@codesplitters.com>
2022-09-26 15:44:32 +0200

    Add presenter

---
+++ EssentialFeediOS/Feed Presentation/FeedImagePresenter.swift
@@ -0,0 +1,55 @@
+//
+//  FeedImagePresenter.swift
+//  EssentialFeediOS
+//
+//  Created by Marko Vukušić on 22.09.2022.
+//
+
+import Foundation
+import EssentialFeed
+
+protocol FeedImageView {
+    associatedtype Image
+    
+    func display(_ model: FeedImageViewModel<Image>)
+}
+
+final class FeedImagePresenter<View: FeedImageView, Image> where View.Image == Image {
+    private let view: View
+    private let imageTransformer: (Data) -> Image?
+    
+    init(view: View, imageTransformer: @escaping (Data) -> Image?) {
+        self.view = view
+        self.imageTransformer = imageTransformer
+    }
+    
+    private struct InvalidImageDataError: Error {}
+    
+    func didStartLoadingImageData(for model: FeedImage) {
+        view.display(FeedImageViewModel(description: model.description,
+                                        location: model.location,
+                                        image: nil,
+                                        isLoading: true,
+                                        shouldRetry: false))
+    }
+    
+    func didFinishLoadingImageData(with data: Data, for model: FeedImage) {
+        guard let image = imageTransformer(data) else {
+            return didFinishLoadingImageData(with: InvalidImageDataError(), for: model)
+        }
+        
+        view.display(FeedImageViewModel(description: model.description,
+                                        location: model.location,
+                                        image: image,
+                                        isLoading: false,
+                                        shouldRetry: false))
+    }
+    
+    func didFinishLoadingImageData(with error: Error, for model: FeedImage) {
+        view.display(FeedImageViewModel(description: model.description,
+                                        location: model.location,
+                                        image: nil,
+                                        isLoading: false,
+                                        shouldRetry: true))
+    }
+}
