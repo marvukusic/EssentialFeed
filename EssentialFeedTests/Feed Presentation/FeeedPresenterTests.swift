50a17ad0834c5fe3c6fc2ada1ade6d4df3bfc90f
Marko Vukušić <marko@codesplitters.com>
2022-10-26 11:11:18 +0200

    Create makeSUT method

--- EssentialFeedTests/Feed Presentation/FeeedPresenterTests.swift
+++ EssentialFeedTests/Feed Presentation/FeeedPresenterTests.swift
@@ -29,5 +29,15 @@ final class FeeedPresenterTests: XCTestCase {
     private class ViewSpy {
         let messages = [Any]()
     }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: FeedPresenter, view: ViewSpy) {
+        let view = ViewSpy()
+        let sut = FeedPresenter(view: view)
+        trackForMemoryLeaks(view, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, view)
+    }
 
 }
