228d5315f0470c5893e153be065688e1fd915a44
Marko Vukušić <marko@codesplitters.com>
2022-10-26 13:53:15 +0200

    Test title localisation

--- EssentialFeedTests/Feed Presentation/FeeedPresenterTests.swift
+++ EssentialFeedTests/Feed Presentation/FeeedPresenterTests.swift
@@ -45,6 +45,13 @@ class FeedPresenter {
     private let loadingView: FeedLoadingView
     private let errorView: FeedErrorView
     
+    static var title: String {
+        NSLocalizedString("FEED_VIEW_TITLE",
+                          tableName: "Feed",
+                          bundle: Bundle(for: FeedPresenter.self),
+                          comment: "Title for the feed view")
+    }
+    
     private var feedLoadError: String {
         NSLocalizedString("FEED_VIEW_CONNECTION_ERROR",
                           tableName: "Feed",
@@ -75,6 +82,10 @@ class FeedPresenter {
 }
 
 final class FeeedPresenterTests: XCTestCase {
+    
+    func test_title_isLocalized() {
+        XCTAssertEqual(FeedPresenter.title, localized("FEED_VIEW_TITLE"))
+    }
 
     func test_init_doesNotSendMessagesToView() {
         let (_, view) = makeSUT()
