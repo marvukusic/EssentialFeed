8133808da588b2e4a2790b5a403f4f7ad8a22ba5
Marko Vukušić <marko@codesplitters.com>
2023-01-16 12:29:26 +0100

    iOS 16.1+ fix: Set explicit test images scale to 1 to ensure they all have the same scale (so their pngData can be compared pixel by pixel)

--- EssentialFeediOSTests/Feed UI/FeedUIIntegrationTests.swift
+++ EssentialFeediOSTests/Feed UI/FeedUIIntegrationTests.swift
@@ -163,21 +163,22 @@ class FeedUIIntegrationTests: XCTestCase {
         let (sut, loader) = makeSUT()
         
         sut.loadViewIfNeeded()
-        loader.completeFeedLoading(with: [makeImage(), makeImage()], at: 0)
+        loader.completeFeedLoading(with: [makeImage(), makeImage()])
         
         let view0 = sut.simulateFeedImageViewVisible(at: 0)
         let view1 = sut.simulateFeedImageViewVisible(at: 1)
-        XCTAssertEqual(view0?.renderedImage, .none)
-        XCTAssertEqual(view1?.renderedImage, .none)
+        XCTAssertEqual(view0?.renderedImage, .none, "Expected no image for first view while loading first image")
+        XCTAssertEqual(view1?.renderedImage, .none, "Expected no image for second view while loading first image")
         
-        let imageData0 = anyImageData()
+        let imageData0 = UIImage.make(withColor: .red).pngData()!
         loader.completeImageLoading(with: imageData0, at: 0)
-        XCTAssertEqual(view0?.renderedImage, imageData0)
-        XCTAssertEqual(view1?.renderedImage, .none)
-        let imageData1 = anyImageData()
+        XCTAssertEqual(view0?.renderedImage, imageData0, "Expected image for first view once first image loading completes successfully")
+        XCTAssertEqual(view1?.renderedImage, .none, "Expected no image state change for second view once first image loading completes successfully")
+        
+        let imageData1 = UIImage.make(withColor: .blue).pngData()!
         loader.completeImageLoading(with: imageData1, at: 1)
-        XCTAssertEqual(view0?.renderedImage, imageData0)
-        XCTAssertEqual(view1?.renderedImage, imageData1)
+        XCTAssertEqual(view0?.renderedImage, imageData0, "Expected no image state change for first view once second image loading completes successfully")
+        XCTAssertEqual(view1?.renderedImage, imageData1, "Expected image for second view once second image loading completes successfully")
     }
     
     func test_feedImageViewRetryButton_isVisibleOnImageURLLoadError() {
