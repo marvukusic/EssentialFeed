c5112dc135fa418941d4dbbd1adef2ee24994720
Marko Vukušić <marko@codesplitters.com>
2023-03-08 12:40:57 +0100

    Make LoadResourcePresenter generic over the Resource types

--- EssentialFeedTests/Shared Presentation/LoadResourcePresenterTests.swift
+++ EssentialFeedTests/Shared Presentation/LoadResourcePresenterTests.swift
@@ -41,10 +41,11 @@ final class LoadResourcePresenterTests: XCTestCase {
     }
     
     // MARK: - Helpers
+    private typealias SUT = LoadResourcePresenter<String, ViewSpy>
     
-    private func makeSUT(mapper: @escaping LoadResourcePresenter.Mapper = { _ in "any"}, file: StaticString = #file, line: UInt = #line) -> (sut: LoadResourcePresenter, view: ViewSpy) {
+    private func makeSUT(mapper: @escaping SUT.Mapper = { _ in "any"}, file: StaticString = #file, line: UInt = #line) -> (sut: SUT, view: ViewSpy) {
         let view = ViewSpy()
-        let sut = LoadResourcePresenter(resourceView: view, loadingView: view, errorView: view, mapper: mapper)
+        let sut = SUT(resourceView: view, loadingView: view, errorView: view, mapper: mapper)
         trackForMemoryLeaks(view, file: file, line: line)
         trackForMemoryLeaks(sut, file: file, line: line)
         return (sut, view)
@@ -52,7 +53,7 @@ final class LoadResourcePresenterTests: XCTestCase {
     
     private func localized(_ key: String, file: StaticString = #file, line: UInt = #line) -> String {
         let table = "Feed"
-        let bundle = Bundle(for: LoadResourcePresenter.self)
+        let bundle = Bundle(for: SUT.self)
         let value = bundle.localizedString(forKey: key, value: nil, table: table)
         if (value == key) {
             XCTFail("Missing localized string key: \(key) in table: \(table)", file: file, line: line)
@@ -61,6 +62,7 @@ final class LoadResourcePresenterTests: XCTestCase {
     }
     
     private class ViewSpy: FeedErrorView, FeedLoadingView, ResourceView {
+        typealias ResourceViewModel = String
         
         enum Message: Hashable {
             case display(errorMessage: String?)
