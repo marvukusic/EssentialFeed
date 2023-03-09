8c56d65a8cbd3b2316882d6232c81da2747756f5
Marko Vukušić <marko@codesplitters.com>
2023-03-09 16:46:07 +0100

    Remove unused FeedImagePresenter logic

--- EssentialFeedTests/Feed Presentation/FeedImagePresenterTests.swift
+++ EssentialFeedTests/Feed Presentation/FeedImagePresenterTests.swift
@@ -13,103 +13,10 @@ final class FeedImagePresenterTests: XCTestCase {
     func test_map_cretesViewModel() {
         let image = uniqueImage()
         
-        let viewModel = FeedImagePresenter<ViewSpy, AnyImage>.map(image)
+        let viewModel = FeedImagePresenter.map(image)
         
         XCTAssertEqual(viewModel.description, image.description)
         XCTAssertEqual(viewModel.location, image.location)
     }
-
-    func test_init_doesNotSendMessagesToView() {
-        let (_, view) = makeSUT()
-        
-        XCTAssertTrue(view.messages.isEmpty)
-    }
     
-    func test_didStartLoadingImageData_displaysLoadingImage() {
-        let image = uniqueImage()
-        let (sut, view) = makeSUT()
-    
-        sut.didStartLoadingImageData(for: image)
-        
-        let message = view.messages.first
-        XCTAssertEqual(view.messages.count, 1)
-        XCTAssertEqual(message?.description, image.description)
-        XCTAssertEqual(message?.location, image.location)
-        XCTAssertEqual(message?.isLoading, true)
-        XCTAssertEqual(message?.shouldRetry, false)
-        XCTAssertNil(message?.image)
-    }
-    
-    func test_didFinishLoadingImageData_displaysRetryOnFailedImageTransformation() {
-        let image = uniqueImage()
-        let (sut, view) = makeSUT(imageTransformer: fail)
-        
-        sut.didFinishLoadingImageData(with: Data(), for: image)
-        
-        let message = view.messages.first
-        XCTAssertEqual(view.messages.count, 1)
-        XCTAssertEqual(message?.description, image.description)
-        XCTAssertEqual(message?.location, image.location)
-        XCTAssertEqual(message?.isLoading, false)
-        XCTAssertEqual(message?.shouldRetry, true)
-        XCTAssertNil(message?.image)
-    }
-    
-    func test_didFinishLoadingImageData_displaysImageOnSuccessfulTransformation() {
-        let image = uniqueImage()
-        let transformedData = AnyImage()
-        let (sut, view) = makeSUT(imageTransformer: { _ in transformedData })
-        
-        sut.didFinishLoadingImageData(with: Data(), for: image)
-        
-        let message = view.messages.first
-        XCTAssertEqual(view.messages.count, 1)
-        XCTAssertEqual(message?.description, image.description)
-        XCTAssertEqual(message?.location, image.location)
-        XCTAssertEqual(message?.isLoading, false)
-        XCTAssertEqual(message?.shouldRetry, false)
-        XCTAssertEqual(message?.image, transformedData)
-    }
-    
-    func test_didFinishLoadingImageDataWithError_displaysRetry() {
-        let image = uniqueImage()
-        let error = anyNSError()
-        let (sut, view) = makeSUT()
-        
-        sut.didFinishLoadingImageData(with: error, for: image)
-        
-        let message = view.messages.first
-        XCTAssertEqual(view.messages.count, 1)
-        XCTAssertEqual(message?.description, image.description)
-        XCTAssertEqual(message?.location, image.location)
-        XCTAssertEqual(message?.isLoading, false)
-        XCTAssertEqual(message?.shouldRetry, true)
-        XCTAssertEqual(message?.image, nil)
-    }
-    
-    class ViewSpy: FeedImageView {
-        typealias Image = AnyImage
-        
-        var messages = [FeedImageViewModel<Image>]()
-
-        func display(_ model: FeedImageViewModel<Image>) {
-            messages.append(model)
-        }
-    }
-    
-    // MARK: - Helpers
-    
-    private func makeSUT(imageTransformer: @escaping (Data) -> AnyImage? = { _ in nil }, file: StaticString = #file, line: UInt = #line) -> (sut: FeedImagePresenter<ViewSpy, AnyImage>, view: ViewSpy) {
-        let view = ViewSpy()
-        let sut = FeedImagePresenter(view: view, imageTransformer: imageTransformer)
-        trackForMemoryLeaks(view, file: file, line: line)
-        trackForMemoryLeaks(sut, file: file, line: line)
-        return (sut, view)
-    }
-    
-    struct AnyImage: Equatable {}
-    
-    private var fail: (Data) -> AnyImage? {
-        return { _ in nil }
-    }
 }
