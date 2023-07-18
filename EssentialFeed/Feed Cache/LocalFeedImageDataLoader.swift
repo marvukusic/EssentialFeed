bcc27db5e89c3899764dac0776c3cd2ac7d1075d
Marko Vukušić <marko@codesplitters.com>
2023-07-18 14:30:51 +0200

    Make FeedImageDataLoader sync

--- EssentialFeed/Feed Cache/LocalFeedImageDataLoader.swift
+++ EssentialFeed/Feed Cache/LocalFeedImageDataLoader.swift
@@ -14,24 +14,21 @@ public final class LocalFeedImageDataLoader {
         self.store = store
     }
     
-    private final class LoadImageDataTask: FeedImageDataLoaderTask {
-        private var completion: ((FeedImageDataLoader.Result) -> Void)?
-        
-        init(_ completion: @escaping (FeedImageDataLoader.Result) -> Void) {
-            self.completion = completion
+    public func loadImageData(from url: URL) throws -> Data {
+        do {
+            if let imageData = try store.retrieve(dataForURL: url) {
+                return imageData
+            }
+        } catch {
+            throw LoadError.failed
         }
         
-        func complete(with result: FeedImageDataLoader.Result) {
-            completion?(result)
-        }
-        
-        func cancel() {
-            preventFurtherCompletions()
-        }
-        
-        private func preventFurtherCompletions() {
-            completion = nil
-        }
+        throw LoadError.notFound
+    }
+    
+    public enum LoadError: Error {
+        case failed
+        case notFound
     }
 }
 
@@ -46,28 +43,5 @@ extension LocalFeedImageDataLoader: FeedImageDataCache {
         } catch {
             throw SaveError.failed
         }
-    }
-}
-
- extension LocalFeedImageDataLoader: FeedImageDataLoader {
-     public typealias LoadResult = FeedImageDataLoader.Result
-
-     public enum LoadError: Error {
-        case failed
-        case notFound
-    }
-    
-     public func loadImageData(from url: URL, completion: @escaping (LoadResult) -> Void) -> FeedImageDataLoaderTask {
-         let task = LoadImageDataTask(completion)
-         task.complete(
-            with: Swift.Result {
-                try store.retrieve(dataForURL: url)
-            }
-                .mapError { _ in LoadError.failed }
-                .flatMap { data in
-                    data.map { .success($0) } ?? .failure(LoadError.notFound)
-                })
-
-        return task
     }
 }
